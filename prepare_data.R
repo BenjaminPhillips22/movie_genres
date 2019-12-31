4a3d3e84df8ca7a2128305e2de97f7a34896b3f8
Ben Phillips <BPhillips@bunnings.com.au>
2019-12-31 14:51:42 +1100

    looked at genres

--- prepare_data.R
+++ prepare_data.R
@@ -1,6 +1,8 @@
 
 library(data.table)
 library(magrittr)
+library(ggplot2)
+library(ruimtehol)
 
 ## Functions ---------------------------
 
@@ -92,4 +94,99 @@ saveRDS(object = movies, file = "movie_plot_genre.rds")
 movies <- readRDS(file = "movie_plot_genre.rds")
 
 
+## EDA --------------------------
+
+# look at the distribution of genres
+sapply(X = movies[1:5], 
+       FUN = function(x){
+           strsplit(x$Genres, split = ',')[[1]] %>% 
+               paste(., x$ID, sep = '-')
+       }) %>%
+    unlist(x = ., use.names = FALSE)
+
+
+apply(X = movies[1:5], MARGIN = 1, 
+      FUN = function(x){
+          strsplit(x$Genres, split = ',')[[1]] #%>% 
+              # paste(., x$ID, sep = '-')
+      }) %>%
+    unlist(x = ., use.names = FALSE)
+
+genres_tmp1 <- mapply(function(Genres, Title){
+    FUN = strsplit(Genres, split = ',')[[1]] %>% 
+        paste(., Title, sep = ',,,')},
+    movies$Genres, movies$Title) %>% 
+    unlist(x = ., use.names = FALSE) %>% 
+    strsplit(., split = ',,,') 
+
+genres_tmp2 <- sapply(genres_tmp1, FUN = function(x){x[1]})
+genres_tmp3 <- sapply(genres_tmp1, FUN = function(x){x[2]})
+
+genres_df <- data.table(Title = genres_tmp3, Genre = genres_tmp2)
+
+genres_df %>% View()                      
+
+# how many movies?
+dim(movies)
+# [1] 42207     5
+length(unique(genres_df$Title))
+# [1] 39917
+
+# so might have some repeat movies titles...
+length(unique(movies$Title))
+# [1] 39917
+
+# what are the repeated movies?
+movies[duplicated(movies$Title) | duplicated(movies$Title, fromLast=TRUE)] %>% 
+    View()
+
+movies[duplicated(movies$Title) | duplicated(movies$Title, fromLast=TRUE), Title] %>% 
+    unique(.) %>% 
+    length()
+# [1] 1830
+
+# 1830 repeated movie titles. some are different movies with the same title, most are
+# the same movie. Due to the general inconsistancy that the movies are labelled,
+# I am happy to leave those repeated movies in, as the have different plot descriptions,
+# even for the same movie, and different genres.
+
+# what is the distribution of number of genres per movie?
+
+num_genre_df <- genres_df %>%
+    .[, .(num_genres = .N), by = Title] %>% 
+    .[order(-num_genres)]
+    
+num_genre_df %>% View()
+
+g1 <- ggplot(data = num_genre_df, 
+             mapping = aes(x = num_genres)) +
+    geom_histogram(binwidth = 1)
+g1
+
+g2 <- ggplot(data = num_genre_df, 
+             mapping = aes(y = num_genres)) +
+    geom_boxplot()
+g2
+
+# let's look at the genres for the top 5 movies with the most genres
+num_genre_df %>% 
+    head(., 5) %>% 
+    merge.data.table(x = .,
+                     y = movies,
+                     by.x = "Title", 
+                     by.y = "Title") %>% 
+    View(.)
+
+# the outliers are due to repeated film titles.
+
+# let's look at the genres
+
+popular_genres <- genres_df %>% 
+    .[, .(count = .N), by = Genre] %>% 
+    .[order(-count)] %>% 
+    .[, cumsum := cumsum(count)/sum(count)]
+
+popular_genres %>% View
+# The top 10 genres account for 46% of the genres
+# The top 74 genres account for 90% of the genres
 
